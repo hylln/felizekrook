d90fa0d5ff57ff6299f1dbd5411333ae77c32417
hylln <linus.hull33@gmail.com>
2026-08-20 22:37:47 +0200

    refactor: remove node adapter and conditionally load keystatic integration and redirects based on command

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import keystatic from '@keystatic/astro';
-import node from '@astrojs/node';
 import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({
-  adapter: node({
-    mode: 'standalone',
-  }),
-  integrations: [react(), keystatic()],
-  redirects: {
+export default defineConfig(({ command }) => ({
+  integrations: [
+    react(),
+    command === 'dev' ? keystatic() : [],
+  ],
+  redirects: command === 'dev' ? {
     '/admin': '/keystatic',
-  },
-});
+  } : {},
+}));
